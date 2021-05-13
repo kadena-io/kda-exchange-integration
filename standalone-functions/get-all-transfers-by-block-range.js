423095e6142ea7c44cc2444a37450c836e643f8b
fmelp <melpignanof@gmail.com>
2021-05-12 20:31:45 -0400

    simpler return

--- standalone-functions/get-all-transfers-by-block-range.js
+++ standalone-functions/get-all-transfers-by-block-range.js
@@ -3,10 +3,35 @@ var {
   getDecimal
 } = require('../util/blockchain-helpers.js')
 
+/**
+ * Gets all transfers (same and cross chain) on a given chain for a given blockheight range
+ * @param chainId {string} - this is the chain you are listening for transfers on
+ * @param startBlock {int} - block height to start looking
+ * @param endBlock {int} - block height to end looking
+ * @return {array[obj]} in this format
+    [
+     {
+       from: 'd250a56932b4407c0630e03feb4a126ad5d3bd2f754b424847b32c665f69fb1a',
+       to: '394a97e0c49e38104972e74d63f12dd9841d19a32deb4efc96fdafa4000f61f6',
+       amount: 1250,
+       type: 'same-chain',
+       height: 1615481,
+       reqKey: '7CaYXYHKaP7xkZYsyWLkOZ9M7jTjjJo116DL0F4N51c'
+     },
+     {
+       from: '9dfdf6d27f05ecea60c8a98311755ad93f91659e0c0e29402aabb0131ed78da7',
+       to: '9dfdf6d27f05ecea60c8a98311755ad93f91659e0c0e29402aabb0131ed78da7',
+       height: 1615481,
+       amount: 6,
+       type: 'cross-chain',
+       reqKey: 'p3cWibImAgUlIiR_7wBVRNBtfYuUn5_7HiNldMQXYIQ'
+     },
+     ...,
+   ]
+**/
 const getTransfers = async (chainId, startBlock, endBlock) => {
   let txs = await chainweb.transactions.range(chainId, startBlock, endBlock);
-  const sameChainTransfers = [];
-  const crossChainTransfers = [];
+  const transfers = [];
   for (let i = 0; i < txs.length; i++) {
     if (txs[i].output.result.status === "success") {
       let events = txs[i].output.events;
@@ -14,12 +39,13 @@ const getTransfers = async (chainId, startBlock, endBlock) => {
         //check if tx has transfer events
         for (let j = 0; j < events.length; j++) {
           if (events[j].name === "TRANSFER" && events[j].module.name === "coin") {
-            sameChainTransfers.push(
+            transfers.push(
               {
                 "from": events[j].params[0],
                 "to": events[j].params[1],
                 "amount": getDecimal(events[j].params[2]),
                 "type": "same-chain",
+                "height": txs[i].height,
                 "reqKey": txs[i].output.reqKey
               }
             )
@@ -29,10 +55,11 @@ const getTransfers = async (chainId, startBlock, endBlock) => {
         //not a direct transfer
         if (txs[i].output.continuation) {
           if (txs[i].output.continuation.continuation.def === 'coin.transfer-crosschain') {
-            crossChainTransfers.push(
+            transfers.push(
               {
                 "from": txs[i].output.continuation.continuation.args[0],
                 "to": txs[i].output.continuation.continuation.args[1],
+                "height": txs[i].height,
                 "amount": getDecimal(txs[i].output.continuation.continuation.args[4]),
                 "type": "cross-chain",
                 "reqKey": txs[i].output.reqKey
@@ -52,14 +79,9 @@ const getTransfers = async (chainId, startBlock, endBlock) => {
       continue;
     }
   }
-  console.log(sameChainTransfers)
-  console.log(crossChainTransfers)
-  return {
-    sameChainTransfers,
-    crossChainTransfers
-  }
+  console.log(transfers)
+  return transfers
 }
 
-//all transfers happened on chain 1
-//from block 1613200 to block 1613308
-getTransfers(1, 1613200, 1613308)
+
+getTransfers(0, 1615447, 1615482)
